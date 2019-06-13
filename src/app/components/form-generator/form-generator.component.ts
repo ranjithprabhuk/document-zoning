cb7c82dab8903a79d82bab1fdbe9ae33e4af15b4
Ranjithprabhu <ranjithprabhu.kumar@infosys.com>
2019-06-13 10:45:13 +0530

    add a null check to avoid the run time error

--- src/app/components/form-generator/form-generator.component.ts
+++ src/app/components/form-generator/form-generator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { Component, OnInit, ComponentFactoryResolver, ViewChild, AfterViewChecked } from '@angular/core';
 import { FormDirective } from '../form.directive';
 import { FormItem } from '../../shared/models/form-item';
 import { FormService } from '../../shared/services/form.service';
@@ -9,7 +9,7 @@ import { FormComponent } from '../../shared/models/form';
   templateUrl: './form-generator.component.html',
   styleUrls: ['./form-generator.component.scss']
 })
-export class FormGeneratorComponent implements OnInit {
+export class FormGeneratorComponent implements OnInit, AfterViewChecked {
   @ViewChild(FormDirective) formHost: FormDirective;
 
   constructor(
@@ -19,17 +19,27 @@ export class FormGeneratorComponent implements OnInit {
 
     }
 
-  ngOnInit() {
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewChecked(): void {
+    this.generateForm();
+  }
+
+  public generateForm(): void {
     const form = this.formService.getForm();
-    const viewContainerRef = this.formHost.viewContainerRef;
-    viewContainerRef.clear();
-    form.forEach(comp => {
-      const componentFactory = this.componentFactoryResolver.resolveComponentFactory(comp.component);
-      const componentRef = viewContainerRef.createComponent(componentFactory);
-      (componentRef.instance as FormComponent).data = comp.data;
-
-    });
-
+    if (this.formHost) {
+      const { viewContainerRef } = this.formHost;
+      if (viewContainerRef) {
+        viewContainerRef.clear();
+        form.forEach(comp => {
+          const componentFactory = this.componentFactoryResolver.resolveComponentFactory(comp.component);
+          const componentRef = viewContainerRef.createComponent(componentFactory);
+          (componentRef.instance as FormComponent).data = comp.data;
+        });
+      }
+    }
   }
 
 }
