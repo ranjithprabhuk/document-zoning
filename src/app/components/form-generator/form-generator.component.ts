98a712a4388ea43006d5f3249c9b325c27617555
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-06-13 22:18:07 +0530

    generate form made asynchronous

--- src/app/components/form-generator/form-generator.component.ts
+++ src/app/components/form-generator/form-generator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ComponentFactoryResolver, ViewChild, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, ComponentFactoryResolver, ViewChild, AfterViewInit } from '@angular/core';
 import { FormDirective } from '../form.directive';
 import { FormItem } from '../../shared/models/form-item';
 import { FormService } from '../../shared/services/form.service';
@@ -9,7 +9,7 @@ import { FormComponent } from '../../shared/models/form';
   templateUrl: './form-generator.component.html',
   styleUrls: ['./form-generator.component.scss']
 })
-export class FormGeneratorComponent implements OnInit, AfterViewChecked {
+export class FormGeneratorComponent implements OnInit, AfterViewInit {
   @ViewChild(FormDirective) formHost: FormDirective;
 
   constructor(
@@ -23,8 +23,8 @@ export class FormGeneratorComponent implements OnInit, AfterViewChecked {
 
   }
 
-  ngAfterViewChecked(): void {
-    this.generateForm();
+  ngAfterViewInit(): void {
+    setTimeout(() => this.generateForm(), 500);
   }
 
   public generateForm(): void {
