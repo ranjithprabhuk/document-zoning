b4174f85296801d6ecf539449913db054f05d7a1
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-06-14 14:42:47 +0530

    form type constants are added

---
+++ src/app/shared/constants/form-type.ts
@@ -0,0 +1,15 @@
+export interface FormType {
+  id: string;
+  value: string;
+}
+
+export const FORM_TYPES: FormType[] = [
+  {
+    id: '1',
+    value: 'medical'
+  },
+  {
+    id: '2',
+    value: 'non-medical'
+  }
+];
