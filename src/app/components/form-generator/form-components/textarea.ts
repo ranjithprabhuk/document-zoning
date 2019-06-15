f304713cd85d00b579d0fe73a81da0477fbc0fbe
Sasi Konduru <sasikumar.konduru@gmail.com>
2019-06-15 11:30:21 +0530

    Add TextArea

--- src/app/components/form-generator/form-components/textarea.ts
+++ src/app/components/form-generator/form-components/textarea.ts
@@ -4,20 +4,12 @@ import { BaseComponent } from './basecomponent';
 
 @Component({
   template: `
-<<<<<<< HEAD
-    <label class="col-12 control-label" for="{{data.labelText}}">{{data.labelText}}</label>
-      <div class="col-12">
-        <input type="input" class="form-control" id="{{data.labelText}}" placeholder="{{data.value}}"
-          (focus)="focusFunction()" (focusout)="focusOutFunction()">
-      </div>
-=======
     <div class="form-group">
     <label class="col-12" for="{{data.labelText}}">{{data.labelText}}</label>
     <div class="col-12">
     <textarea row="3" class="form-control" id="{{data.labelText}}" placeholder="{{data.value}}"
  (focus)="focusFunction()" (focusout)="focusOutFunction()"></textarea>
     <div>
->>>>>>> fb48705e816a89f09c605f30306793a91f56cea4
     </div>
   `
 })
