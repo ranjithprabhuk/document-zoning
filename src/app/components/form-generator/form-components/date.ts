6ec7e69acc312759699b0ed123c17983deed5e1a
Banibrata Roy <banibrataroy87@yahoo.com>
2019-06-15 11:26:47 +0530

    from group in all controls

--- src/app/components/form-generator/form-components/date.ts
+++ src/app/components/form-generator/form-components/date.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedService } from '@shared/shared.service';
 import { BaseComponent } from './basecomponent';
+import { FormComponent } from '../../../shared/models/form';
+import { FormControl } from '@angular/forms';
 
 @Component({
   template: `
-    <div class="form-group">
+    <div class="form-group" [formGroup]="parentGroup">
     <label class="col-sm-2 control-label">{{data.labelText}}</label>
     <div class="col-sm-10">
     <input type="date" class="form-control" id="{{data.labelText}}" placeholder="{{data.value}}"
-    (focus)="focusFunction()" (focusout)="focusOutFunction()">
+    (focus)="focusFunction()" (focusout)="focusOutFunction()" [formControlName]="data.formControlName">
     </div>
     </div>
   `
 })
-export class DateComponent extends BaseComponent {
+export class DateComponent extends BaseComponent implements FormComponent, OnInit {
   constructor(public sharedService: SharedService) {
     super(sharedService);
+  }
+
+  ngOnInit() {
+    const controlName = new FormControl(this.data.value);
+    this.parentGroup.addControl([this.data.formControlName] , controlName);
   }
 }
