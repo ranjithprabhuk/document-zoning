af771f74db65d70b335ecfdd0cf7eecb8c5c0fa7
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-07-03 17:02:12 +0530

    fabric added to the library component module

--- projects/pdf-highlighter/src/lib/pdf-highlighter.module.ts
+++ projects/pdf-highlighter/src/lib/pdf-highlighter.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FabricModule, FABRIC_CONFIG } from 'ngx-fabric-wrapper';
+
 import { PdfHighlighterComponent } from './pdf-highlighter.component';
 
 @NgModule({
   declarations: [PdfHighlighterComponent],
   imports: [
+    FabricModule
+  ],
+  providers: [
+    { provide: FABRIC_CONFIG, useValue: {} },
   ],
   exports: [PdfHighlighterComponent]
 })
