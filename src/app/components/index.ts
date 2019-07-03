bd700f72157f8e2448d391dede72c046880c4a2f
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-07-03 17:00:23 +0530

    removed the pdf highlighter component from the app

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,4 +1,3 @@
 export { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 export { UploadFormComponent } from './upload-form/upload-form.component';
 export { FormGeneratorComponent } from './form-generator/form-generator.component';
-export { PdfHighlighterComponent } from './pdf-highlighter/pdf-highlighter.component';
